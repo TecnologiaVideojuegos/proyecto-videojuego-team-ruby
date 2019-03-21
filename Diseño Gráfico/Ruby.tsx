f60a18b1fe884d999f781ac71cce62e6ffb7712c
Marina <46962469+MarinaFer@users.noreply.github.com>
2019-03-21 01:57:38 +0100

    Add files via upload

---
+++ Diseño Gráfico/Ruby.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset version="1.2" tiledversion="1.2.3" name="Ruby" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="../Ruby.png" width="102" height="144"/>
+ <terraintypes>
+  <terrain name="01" tile="0"/>
+  <terrain name="02" tile="1"/>
+  <terrain name="03" tile="2"/>
+  <terrain name="04" tile="3"/>
+  <terrain name="05" tile="4"/>
+  <terrain name="06" tile="5"/>
+  <terrain name="07" tile="6"/>
+  <terrain name="08" tile="7"/>
+  <terrain name="09" tile="8"/>
+  <terrain name="10" tile="9"/>
+  <terrain name="11" tile="10"/>
+  <terrain name="12" tile="11"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="1,1,1,1"/>
+ <tile id="2" terrain="2,2,2,2"/>
+ <tile id="3" terrain="3,3,3,3"/>
+ <tile id="4" terrain="4,4,4,4"/>
+ <tile id="5" terrain="5,5,5,5"/>
+ <tile id="6" terrain="6,6,6,6"/>
+ <tile id="7" terrain="7,7,7,7"/>
+ <tile id="8" terrain="8,8,8,8"/>
+ <tile id="9" terrain="9,9,9,9"/>
+ <tile id="10" terrain="10,10,10,10"/>
+ <tile id="11" terrain="11,11,11,11"/>
+</tileset>
