25b24e5907db7792112c21c7653c04efc1becd75
Marina <46962469+MarinaFer@users.noreply.github.com>
2019-04-16 00:17:13 +0200

    Texturas del mapa

---
+++ Diseño Gráfico/Paquete de texturas/Arboles.tsx
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset version="1.2" tiledversion="1.2.3" name="Arboles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="RPGMaker/Utilizados/Arboles.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="A01" tile="240"/>
+  <terrain name="A02" tile="241"/>
+  <terrain name="A03" tile="242"/>
+  <terrain name="A04" tile="243"/>
+  <terrain name="A05" tile="224"/>
+  <terrain name="A06" tile="225"/>
+  <terrain name="A07" tile="226"/>
+  <terrain name="A08" tile="227"/>
+  <terrain name="A09" tile="208"/>
+  <terrain name="A10" tile="209"/>
+  <terrain name="A11" tile="210"/>
+  <terrain name="A12" tile="211"/>
+  <terrain name="A13" tile="192"/>
+  <terrain name="A14" tile="193"/>
+  <terrain name="A15" tile="194"/>
+  <terrain name="A16" tile="195"/>
+  <terrain name="B01" tile="244"/>
+  <terrain name="B02" tile="245"/>
+  <terrain name="B03" tile="246"/>
+  <terrain name="B04" tile="228"/>
+  <terrain name="B05" tile="229"/>
+  <terrain name="B06" tile="230"/>
+  <terrain name="B07" tile="212"/>
+  <terrain name="B08" tile="213"/>
+  <terrain name="B09" tile="214"/>
+  <terrain name="B10" tile="196"/>
+  <terrain name="B11" tile="197"/>
+  <terrain name="B12" tile="198"/>
+  <terrain name="C01" tile="90"/>
+  <terrain name="C02" tile="91"/>
+  <terrain name="C03" tile="92"/>
+  <terrain name="C04" tile="74"/>
+  <terrain name="C05" tile="75"/>
+  <terrain name="C06" tile="76"/>
+  <terrain name="C07" tile="58"/>
+  <terrain name="C08" tile="59"/>
+  <terrain name="C09" tile="60"/>
+  <terrain name="Arbusto 1" tile="231"/>
+  <terrain name="Arbusto 2" tile="232"/>
+  <terrain name="Arbusto 3" tile="248"/>
+  <terrain name="Tomates" tile="148"/>
+  <terrain name="Berenjenas" tile="132"/>
+  <terrain name="Cebollas" tile="56"/>
+  <terrain name="Setas" tile="27"/>
+  <terrain name="Flor 1" tile="29"/>
+  <terrain name="Flor 2" tile="11"/>
+  <terrain name="Flor 3" tile="10"/>
+  <terrain name="Flor 4" tile="26"/>
+  <terrain name="Flor 5" tile="13"/>
+  <terrain name="Flor 6" tile="12"/>
+  <terrain name="Tocón" tile="215"/>
+  <terrain name="Roca" tile="62"/>
+ </terraintypes>
+ <tile id="10" terrain="46,46,46,46"/>
+ <tile id="11" terrain="45,45,45,45"/>
+ <tile id="12" terrain="49,49,49,49"/>
+ <tile id="13" terrain="48,48,48,48"/>
+ <tile id="26" terrain="47,47,47,47"/>
+ <tile id="27" terrain="43,43,43,43"/>
+ <tile id="29" terrain="44,44,44,44"/>
+ <tile id="56" terrain="42,42,42,42"/>
+ <tile id="58" terrain="34,34,34,34"/>
+ <tile id="59" terrain="35,35,35,35"/>
+ <tile id="60" terrain="36,36,36,36"/>
+ <tile id="62" terrain="51,51,51,51"/>
+ <tile id="74" terrain="31,31,31,31"/>
+ <tile id="75" terrain="32,32,32,32"/>
+ <tile id="76" terrain="33,33,33,33"/>
+ <tile id="90" terrain="28,28,28,28"/>
+ <tile id="91" terrain="29,29,29,29"/>
+ <tile id="92" terrain="30,30,30,30"/>
+ <tile id="132" terrain="41,41,41,41"/>
+ <tile id="148" terrain="40,40,40,40"/>
+ <tile id="192" terrain="12,12,12,12"/>
+ <tile id="193" terrain="13,13,13,13"/>
+ <tile id="194" terrain="14,14,14,14"/>
+ <tile id="195" terrain="15,15,15,15"/>
+ <tile id="196" terrain="25,25,25,25"/>
+ <tile id="197" terrain="26,26,26,26"/>
+ <tile id="198" terrain="27,27,27,27"/>
+ <tile id="208" terrain="8,8,8,8"/>
+ <tile id="209" terrain="9,9,9,9"/>
+ <tile id="210" terrain="10,10,10,10"/>
+ <tile id="211" terrain="11,11,11,11"/>
+ <tile id="212" terrain="22,22,22,22"/>
+ <tile id="213" terrain="23,23,23,23"/>
+ <tile id="214" terrain="24,24,24,24"/>
+ <tile id="215" terrain="50,50,50,50"/>
+ <tile id="224" terrain="4,4,4,4"/>
+ <tile id="225" terrain="5,5,5,5"/>
+ <tile id="226" terrain="6,6,6,6"/>
+ <tile id="227" terrain="7,7,7,7"/>
+ <tile id="228" terrain="19,19,19,19"/>
+ <tile id="229" terrain="20,20,20,20"/>
+ <tile id="230" terrain="21,21,21,21"/>
+ <tile id="231" terrain="37,37,37,37"/>
+ <tile id="232" terrain="38,38,38,38"/>
+ <tile id="240" terrain="0,0,0,0"/>
+ <tile id="241" terrain="1,1,1,1"/>
+ <tile id="242" terrain="2,2,2,2"/>
+ <tile id="243" terrain="3,3,3,3"/>
+ <tile id="244" terrain="16,16,16,16"/>
+ <tile id="245" terrain="17,17,17,17"/>
+ <tile id="246" terrain="18,18,18,18"/>
+ <tile id="248" terrain="39,39,39,39"/>
+</tileset>
---
+++ Diseño Gráfico/Paquete de texturas/Suelo.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset version="1.2" tiledversion="1.2.3" name="Suelo" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="RPGMaker/Utilizados/Suelo.png" width="512" height="384"/>
+ <terraintypes>
+  <terrain name="Hierba" tile="0"/>
+  <terrain name="Hierba 2" tile="97"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="97" terrain="1,1,1,1"/>
+</tileset>
