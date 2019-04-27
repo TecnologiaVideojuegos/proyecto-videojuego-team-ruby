2697ff1e49155ae3a1d2c2526bd2805e0799c89e
Marina <46962469+MarinaFer@users.noreply.github.com>
2019-04-27 19:42:02 +0200

    Upload Sprites Plantas de fuego

---
+++ Diseño Gráfico/Paquete de texturas/Sprites Plantas.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset version="1.2" tiledversion="1.2.3" name="sdfgsdgsd" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="sdfgsdgsd.png" width="156" height="165"/>
+</tileset>
