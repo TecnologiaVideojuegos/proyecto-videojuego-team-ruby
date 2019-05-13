ce3f4e005dc87c88d1caf39e5b9f9f5a79ec890a
KinGJV <jesusvillafranca007@gmail.com>
2019-05-13 17:39:04 +0200

    subidos los .png y .tsx

---
+++ Ruby/resources/maps/Nivel 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Nivel 1" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="Nivel 1.png" width="1024" height="1024"/>
+</tileset>
---
+++ Ruby/resources/maps/Nivel_1X2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Nivel_1-lanczos3 (1)" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="Nivel1_X2.png" width="2048" height="2048"/>
+</tileset>
