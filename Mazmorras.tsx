9eedf5e2ae919e7c90d113d028c041be61363ad9
KinGJV <jesusvillafranca007@gmail.com>
2019-05-15 08:40:15 +0200

    Delete Mazmorras.tsx

--- Mazmorras.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Mazmorras" tilewidth="32" tileheight="32" tilecount="12" columns="3">
- <image source="Mazmorras.png" width="96" height="128"/>
-</tileset>
