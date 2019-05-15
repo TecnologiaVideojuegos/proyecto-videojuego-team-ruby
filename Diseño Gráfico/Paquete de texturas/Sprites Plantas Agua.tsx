eb842d3a50246f701975ed8f311c0ab0ec8993f9
Marina <46962469+MarinaFer@users.noreply.github.com>
2019-05-15 17:26:10 +0200

    Add files via upload

---
+++ Diseño Gráfico/Paquete de texturas/Sprites Plantas Agua.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Sprites Plantas Agua" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="Sprites Plantas Agua.png" width="156" height="165"/>
+</tileset>
