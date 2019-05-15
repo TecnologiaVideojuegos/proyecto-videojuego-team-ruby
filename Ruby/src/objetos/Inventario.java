497fb5b5dbe52ee82c7a5431007dfc3ae3d679ed
Enrique Coronado <enriquecoronado96@gmail.com>
2019-05-15 13:03:46 +0200

    Arreglado el bug del guardado

--- Ruby/src/objetos/Inventario.java
+++ Ruby/src/objetos/Inventario.java
@@ -6,6 +6,7 @@ import objetos.pociones.Pocion;
 import objetos.semillas.Semilla;
 
 public class Inventario {
+
     private ArrayList<Planta> plantas;
     private ArrayList<Semilla> semillas;
     private ArrayList<Pocion> pociones;
@@ -21,62 +22,64 @@ public class Inventario {
         Objeto objeto_temp = null;
         int j = 0;
 
-        if (objeto.tipoObjeto().equals("Planta")) {
-            for (Planta objeto_inventario : plantas) {
-                if (objeto_inventario.equals(objeto)) {
-                    anadir = false;
-                    objeto_temp = objeto_inventario;
-                    break;
+        if (i > 0) {    //Si se añade al menos un elemento
+            if (objeto.tipoObjeto().equals("Planta")) {
+                for (Planta objeto_inventario : plantas) {
+                    if (objeto_inventario.equals(objeto)) {
+                        anadir = false;
+                        objeto_temp = objeto_inventario;
+                        break;
+                    }
+                    j++;
                 }
-                j++;
-            }
-            if (!anadir) {
-                objeto_temp.aumentarCantidad(i);
-                plantas.set(j, (Planta) objeto_temp);
-            } else {
-                objeto_temp = objeto;
-                objeto_temp.setCantidad(i);
-                plantas.add((Planta) objeto_temp);
-            }
-        } else if (objeto.tipoObjeto().endsWith("Semilla")) {
-            for (Semilla objeto_inventario : semillas) {
-                if (objeto_inventario.equals(objeto)) {
-                    anadir = false;
-                    objeto_temp = objeto_inventario;
-                    break;
+                if (!anadir) {
+                    objeto_temp.aumentarCantidad(i);
+                    plantas.set(j, (Planta) objeto_temp);
+                } else {
+                    objeto_temp = objeto;
+                    objeto_temp.setCantidad(i);
+                    plantas.add((Planta) objeto_temp);
                 }
-                j++;
-            }
-            if (!anadir) {
-                objeto_temp.aumentarCantidad(i);
-                semillas.set(j, (Semilla) objeto_temp);
-            } else {
-                objeto_temp = objeto;
-                objeto_temp.setCantidad(i);
-                semillas.add((Semilla) objeto_temp);
-            }
-        } else if (objeto.tipoObjeto().equals("Pocion")) {
-            for (Pocion objeto_inventario : pociones) {
-                if (objeto_inventario.equals(objeto)) {
-                    anadir = false;
-                    objeto_temp = objeto_inventario;
-                    break;
+            } else if (objeto.tipoObjeto().endsWith("Semilla")) {
+                for (Semilla objeto_inventario : semillas) {
+                    if (objeto_inventario.equals(objeto)) {
+                        anadir = false;
+                        objeto_temp = objeto_inventario;
+                        break;
+                    }
+                    j++;
                 }
-                j++;
-            }
-            if (!anadir) {
-                objeto_temp.aumentarCantidad(i);
-                pociones.set(j, (Pocion) objeto_temp);
+                if (!anadir) {
+                    objeto_temp.aumentarCantidad(i);
+                    semillas.set(j, (Semilla) objeto_temp);
+                } else {
+                    objeto_temp = objeto;
+                    objeto_temp.setCantidad(i);
+                    semillas.add((Semilla) objeto_temp);
+                }
+            } else if (objeto.tipoObjeto().equals("Pocion")) {
+                for (Pocion objeto_inventario : pociones) {
+                    if (objeto_inventario.equals(objeto)) {
+                        anadir = false;
+                        objeto_temp = objeto_inventario;
+                        break;
+                    }
+                    j++;
+                }
+                if (!anadir) {
+                    objeto_temp.aumentarCantidad(i);
+                    pociones.set(j, (Pocion) objeto_temp);
+                } else {
+                    objeto_temp = objeto;
+                    objeto_temp.setCantidad(i);
+                    pociones.add((Pocion) objeto_temp);
+                }
             } else {
-                objeto_temp = objeto;
-                objeto_temp.setCantidad(i);
-                pociones.add((Pocion) objeto_temp);
+                System.out.println("Error al añadir un objeto al inventario");
             }
-        } else {
-            System.out.println("Error al añadir un objeto al inventario");
         }
     }
-    
+
     /**
      * Elimina uno de cantidad del objeto introducido
      *
@@ -100,7 +103,7 @@ public class Inventario {
                 objeto_temp = plantas.get(j);
                 objeto_temp.reducirCantidad(1);
                 plantas.set(j, (Planta) objeto_temp);
-                if(objeto_temp.getCantidad()==0){
+                if (objeto_temp.getCantidad() == 0) {
                     plantas.remove(j);
                 }
             }
@@ -116,7 +119,7 @@ public class Inventario {
                 objeto_temp = semillas.get(j);
                 objeto_temp.reducirCantidad(1);
                 semillas.set(j, (Semilla) objeto_temp);
-                if(objeto_temp.getCantidad()==0){
+                if (objeto_temp.getCantidad() == 0) {
                     semillas.remove(j);
                 }
             }
@@ -132,7 +135,7 @@ public class Inventario {
                 objeto_temp = pociones.get(j);
                 objeto_temp.reducirCantidad(1);
                 pociones.set(j, (Pocion) objeto_temp);
-                if(objeto_temp.getCantidad()==0){
+                if (objeto_temp.getCantidad() == 0) {
                     pociones.remove(j);
                 }
             }
@@ -141,43 +144,43 @@ public class Inventario {
         }
         return esta;
     }
-    
-    public ArrayList<Planta> getPlantas(){
+
+    public ArrayList<Planta> getPlantas() {
         return plantas;
     }
-    
-    public ArrayList<Semilla> getSemillas(){
+
+    public ArrayList<Semilla> getSemillas() {
         return semillas;
     }
-    
-    public ArrayList<Pocion> getPociones(){
+
+    public ArrayList<Pocion> getPociones() {
         return pociones;
     }
-    
-    public int getCantidad(Objeto objeto){
+
+    public int getCantidad(Objeto objeto) {
         int cantidad = 0;
-        if(objeto.tipoObjeto().equals("Planta")){
-            for(Planta planta: plantas){
-                if(objeto.equals(planta)){
+        if (objeto.tipoObjeto().equals("Planta")) {
+            for (Planta planta : plantas) {
+                if (objeto.equals(planta)) {
                     cantidad = planta.getCantidad();
                     break;
                 }
             }
-        }else if(objeto.tipoObjeto().equals("Semilla")){
-            for(Semilla semilla: semillas){
-                if(objeto.equals(semilla)){
+        } else if (objeto.tipoObjeto().equals("Semilla")) {
+            for (Semilla semilla : semillas) {
+                if (objeto.equals(semilla)) {
                     cantidad = semilla.getCantidad();
                     break;
                 }
             }
-        }else if(objeto.tipoObjeto().equals("Pocion")){
-            for(Pocion pocion: pociones){
-                if(objeto.equals(pocion)){
+        } else if (objeto.tipoObjeto().equals("Pocion")) {
+            for (Pocion pocion : pociones) {
+                if (objeto.equals(pocion)) {
                     cantidad = pocion.getCantidad();
                     break;
                 }
             }
-        }else{
+        } else {
             System.out.println("Error al obtener la cantidad de un objeto");
         }
         return cantidad;
