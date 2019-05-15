936ed02626a099099812902f3c192fd57159fac9
KinGJV <jesusvillafranca007@gmail.com>
2019-05-15 08:40:24 +0200

    Delete MazmorrasX2.tsx

--- MazmorrasX2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="Mazmorras-triangle" tilewidth="32" tileheight="32" tilecount="48" columns="6">
- <image source="Mazmorras_X2.png" width="192" height="256"/>
-</tileset>
