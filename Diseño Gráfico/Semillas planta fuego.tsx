4f5648869e1efc27c050fef13de8707b93eb060f
Marina <46962469+MarinaFer@users.noreply.github.com>
2019-05-16 14:20:40 +0200

    Add semillas

---
+++ Diseño Gráfico/Semillas planta fuego.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Semillas plantafuego" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="Semillas planta fuego.png" width="162" height="136"/>
+</tileset>
